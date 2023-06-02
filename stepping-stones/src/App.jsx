19efcda30df70ab61b682e58cc997bc97f34fedc
Pythonidaer <pythonidaer@gmail.com>
2023-06-01 21:31:46 -0400

    committing changes in attempt to modify stepping-stones folder structure

---
+++ stepping-stones/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import brainLogo from '/brain.svg'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Stepping Stones</h1>
+    </>
+  )
+}
+
+export default App
